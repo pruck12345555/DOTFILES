f029fc6e48031c84675d72822a4c12c718118e3e
pruck12345555 <Deecufs@gmail.com>
2025-12-19 02:14:33 +0700

    initial red commit

---
+++ paste.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONFIG=~/.config
+
+cp $CONFIG/fastfetch/config.jsonc fastfetch
+cp -r $CONFIG/kitty . 
+cp -r $CONFIG/eww   .
+cp -r $CONFIG/hypr  .
+cp -r $CONFIG/mako  .
+cp -r $CONFIG/nvim  .
